6aed47932a74b7d62449543a17ad7a57e7b47175
Tejas <tejasbachhav98@gmail.com>
2020-08-25 18:05:35 +0530

    Day1 started with flutter dev

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First app'),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Text("Hello boys"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('click'),
+        ),
+      ),
+    ));
