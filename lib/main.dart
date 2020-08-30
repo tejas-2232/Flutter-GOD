2ba1f19b2c745eadc71222f507008c5e440efa74
Tejas <tejasbachhav98@gmail.com>
2020-08-30 18:11:13 +0530

    added stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('First app'),
-          centerTitle: true,
-          backgroundColor: Colors.amber,
-        ),
-        body: Center(
-          child: Text(
-            'BMW MOTORS',
-            style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 3.0,
-                color: Colors.grey[600],
-                fontFamily: 'IndieFlower'),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('click'),
-          backgroundColor: Colors.orange,
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First app'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: Text(
+          'BMW MOTORS',
+          style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('add'),
+        backgroundColor: Colors.orange,
+      ),
+    );
+  }
+}
