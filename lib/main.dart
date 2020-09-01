3d6844b3dded025ae1fa52bf2b2074f1068bf082
Tejas <tejasbachhav98@gmail.com>
2020-09-01 23:18:56 +0530

    action button color changed

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,17 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.amber,
       ),
       body: Center(
-        child: Text(
-          'BMW MOTORS',
-          style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower'),
-        ),
-      ),
+          child: Image(
+        image: NetworkImage(
+            'https://images.unsplash.com/photo-1496715976403-7e36dc43f17b?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'),
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('add'),
-        backgroundColor: Colors.orange,
+        backgroundColor: Colors.deepPurple,
       ),
+     
+      )
     );
   }
 }
